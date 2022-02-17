79e71a3fcecb84076ea0fd01480c1f9ef5a951e7
nilsv01 <nils.vogelpohl1@gmail.com>
2022-02-17 10:15:55 +0100

    router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import { render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
 import AppRouter from "./AppRouter";
+import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
-render(
+
+ReactDOM.render(
   <AppRouter/>,
-  rootElement
+   rootElement
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
