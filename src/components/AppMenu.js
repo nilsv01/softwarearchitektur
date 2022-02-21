ad09ddd933b08cab1d887d4d183ef8de34f1c14e
nilsv01 <nils.vogelpohl1@gmail.com>
2022-02-21 15:12:02 +0100

    d

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,12 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './AppMenu.css'
-const AppMenu = (props) => (
-        <ul className='App-Menu'>
-              <div className="App-Menu-Element">
-                 <li><a className="App-Menu-Kunde" href="../">Kunde</a></li>
-                 <li><a className="App-Menu-Mitarbeiter" href="../Mitarbeiter">Mitarbeiter</a></li>
-              </div>
-         </ul>
-)
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { alpha, styled } from '@mui/material/styles';
+import { AlignVerticalTopSharp } from '@mui/icons-material';
+
+
+class AppMenu extends GreenSwitch{
+   state = {
+      checked: true
+    }; 
+   
+   constructor(props){
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      const GreenSwitch = styled(Switch)(({ theme }) => ({
+         '& .MuiSwitch-switchBase.Mui-checked': {
+           color: "#21b6ae",
+           '&:hover': {
+             backgroundColor: alpha("#21b6ae", theme.palette.action.hoverOpacity),
+           },
+         },
+         '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+           backgroundColor: "#21b6ae",
+         },
+       }));
+   }
+
+   handleChange = (event) => {
+      this.setState({checked: event.target.checked});
+   };
+
+
+   render(){
+      return(
+         <AppBar position="static">
+            <Container maxWidth="xl">
+               <Toolbar disableGutters>
+                  <Box>
+                     <FormControlLabel
+                         control={
+                           <Switch
+                            className='UserSwitch'
+                            checked={this.state.checked}
+                            onChange={this.handleChange}
+                            value="checked"
+                            color= 'primary'
+                            variant="contained"
+                           />
+                         }       
+                         labelPlacement="start"                          
+                         label={this.state.checked ? 'Kunde' : 'Mitarbeiter'}     
+                         href={this.state.checked ? '../' : '../Mitarbeiter'}/>
+                  </Box>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                      Busfahrplan
+                  </Typography>
+               </Toolbar>
+            </Container>
+         </AppBar>
+      );
+   }
+}
 
 export default AppMenu
